6294c8f00932a0e50f8693175c719f167a6a9a3f
mzajac <matzaj101@gmail.com>
2019-01-30 00:25:18 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserGetComponent } from './user-get/user-get.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { AppRouting } from './app-routing.module';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { NavigationCancel, Event, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BusinessService } from './services/business.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAddComponent,
+    UserGetComponent,
+    UserEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BusinessService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
